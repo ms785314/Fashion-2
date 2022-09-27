d5d8795ae6e5428b3d319e29a278be22d7e4ca49
ms785314 <ms785314@gmail.com>
2022-09-27 13:44:35 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 
 function App() {
  return (
     <div className="App">
       <Header></Header>
-      <h1 className="underline">Hellos wor</h1>
+      <Routes>
+        <Route path="/" element></Route>
+      </Routes>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,33 @@
+import { faCartShopping, faClover, faHeart, faShoppingBag, faSignIn } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css'
 
 const Header = () => {
+    let wishlistQuantity = 1;
+    let itemsQuantity = 1;
     return (
         <div>
-            <h1>This is header</h1>
+            <nav className='flex justify-between sm:px-20 px-6 py-6 items-center'>
+
+                <div className="logo text-4xl text-gray-600 font-bold">
+
+                    <FontAwesomeIcon style={{ marginRight: '6px' }} icon={faShoppingBag}></FontAwesomeIcon>FASHion</div>
+                <div className="links flex gap-6 text-4xl">
+                    <Link to='/cart' className='relative text-gray-500'>
+                        <span className='absolute h-[18px] w-[18px] rounded-[50%] text-white text-sm text-center right-[-5px] font-bold bg-red-500 top-[-5px] '>{itemsQuantity}</span>
+                        <FontAwesomeIcon icon={faCartShopping}></FontAwesomeIcon>
+                    </Link>
+                    <Link to='/cart' className='relative'>
+                        <span className='absolute h-[18px] w-[18px] rounded-[50%] text-white text-sm text-center right-[-5px] font-bold bg-red-500 top-[-5px] '>{wishlistQuantity}</span>
+                        <FontAwesomeIcon className='text-gray-500' icon={faHeart}></FontAwesomeIcon>
+                    </Link>
+                    <Link to='/cart'>
+                        <FontAwesomeIcon className='text-gray-500' icon={faSignIn}></FontAwesomeIcon>
+                    </Link>
+                </div>
+            </nav>
         </div>
     );
 };
